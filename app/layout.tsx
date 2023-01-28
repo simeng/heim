af6e635940d6096c92f06f20d0be18d7ae88ff2c
Simen Graaten <simeng@gmail.com>
2023-01-28 13:41:03 +0100

    Add viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width" />
         <DefaultTags />
       </head>
       <body>{children}</body>
